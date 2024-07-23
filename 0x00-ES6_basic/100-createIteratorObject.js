0526e0d3c2db99860f005f4b55407f8ee43379ef
ilyassdaaif <ilyass.cougar@gmail.com>
2024-07-23 22:45:23 +0000

    d

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,13 +1,11 @@
 export default function createIteratorObject(report) {
-  const departments = Object.values(report.allEmployees);
-  
-  return {
-    *[Symbol.iterator]() {
-      for (const department of departments) {
-	for (const employee of department) {
-	  yield employee;
-	}
-      }
+  const employees = [];
+
+  for (const department in report.allEmployees) {
+    if (report.allEmployees.hasOwnProperty(department)) {
+      employees.push(...report.allEmployees[department]);
     }
-  };
+  }
+
+  return employees[Symbol.iterator]();
 }
