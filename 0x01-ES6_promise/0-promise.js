ad909d7faa7e47987e76cb8424cf753103592ead
ilyassdaaif <ilyass.cougar@gmail.com>
2024-07-24 18:49:51 +0000

    0x01Es6promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // The Promise doesn't need to do anything specific in this case
+    // It just needs to be a valid Promise object
+  });
+}
+
+export default getResponseFromAPI;
