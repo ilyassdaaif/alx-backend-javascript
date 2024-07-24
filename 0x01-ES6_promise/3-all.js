f9bf5590e894d36024bcb43472f31f0e3e2ae306
ilyassdaaif <ilyass.cougar@gmail.com>
2024-07-24 20:24:44 +0000

    s

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      console.log(
+	`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`
+      );
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
+
+export default handleProfileSignup;
