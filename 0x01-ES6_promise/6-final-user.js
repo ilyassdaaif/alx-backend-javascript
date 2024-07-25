b026496965c6616260c02ae6dfd9d8c3dc15afc8
ilyassdaaif <ilyass.cougar@gmail.com>
2024-07-25 00:13:45 +0000

    s

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,13 +2,22 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
+  return Promise.all([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName)
-  ]).then((results) => {
-    return results.map(result => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason
-    }));
-  });
+  ])
+    .then((results) => {
+     return results.map(result => ({
+       status: 'fulfilled',
+       value: result
+     }));
+   })
+   .catch((error) => {
+     return [
+       {
+	 status: 'rejected',
+	 value: error.toString()
+       }
+     ];
+   });
 }
