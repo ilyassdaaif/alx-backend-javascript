33da10e5c6ac09377da47e80a6946f89481122f5
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-07-25 19:05:45 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Simulate API call
-    resolve('API response');
-  });
+  return new Promise(() => {});
 }
