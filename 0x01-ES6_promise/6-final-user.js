488ca6054f0b0b7e23ff04dfdb64eba95c4b5434
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-07-25 01:20:37 +0100

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,22 +2,19 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
-  ])
-  .then(results => {
-    return results.map(result => ({
-      status: 'fulfilled',
-      value: result
-    }));
-  })
-  .catch(error => {
-    return [
-      {
-        status: 'rejected',
-        value: error
-      }
-    ];
-  });
+  return Promise
+    .all([
+      signUpUser(firstName, lastName),
+      uploadPhoto(fileName)
+    ])
+    .then((results) => {
+      return results.map((result) => ({
+        status: 'fulfilled',
+        value: result,
+      }));
+    })
+    .catch((error) => [
+      { status: 'rejected', value: error.toString() },
+      { status: 'rejected', value: error.toString() },
+    ]);
 }
