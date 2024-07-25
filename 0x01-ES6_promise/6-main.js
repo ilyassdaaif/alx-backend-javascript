9b3baa2f8644794d7d2955d77aec0598058a04b3
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-07-25 03:54:56 +0100

    Update 6-main.js

--- 0x01-ES6_promise/6-main.js
+++ 0x01-ES6_promise/6-main.js
@@ -1,3 +1,3 @@
-import handleProfileSignup from './6-final-user.js';
+import handleProfileSignup from './6-final-user';
 
-handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg").then(console.log);
+console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
