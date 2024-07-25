bc0e9bf4cb1680eeff45efe39b5e044edd99ff5f
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-07-25 19:38:01 +0100

    Update 100-main.js

--- 0x01-ES6_promise/100-main.js
+++ 0x01-ES6_promise/100-main.js
@@ -1,8 +1,15 @@
-import asyncUploadUser from './100-await';
+import { uploadPhoto, createUser } from './utils';
 
-const test = async () => {
-  const value = await asyncUploadUser();
-  console.log(value);
-};
+async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    console.log('Photo and User fetched successfully:', { photo, user });
+    return { photo, user };
+  } catch (error) {
+    console.error('Error occurred while fetching photo or user:', error);
+    return { photo: null, user: null };
+  }
+}
 
-test();
+export default asyncUploadUser;
