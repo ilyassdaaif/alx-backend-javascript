d3f9a5e7f0e8f71114185fc95f3ce4199cca9f45
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-07-25 04:00:18 +0100

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,15 @@
 import { uploadPhoto, createUser } from './utils.js';
 
-export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photo, user]) => {
-      console.log(`${photo} ${user.firstName} ${user.lastName}`);
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      console.log(
+        `${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`
+      );
     })
     .catch(() => {
       console.log('Signup system offline');
     });
 }
+
+export default handleProfileSignup;
