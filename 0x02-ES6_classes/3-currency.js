7293ad2d136f827b8f36d25849057319156e0e26
ilyassdaaif <ilyass.cougar@gmail.com>
2024-07-26 00:21:13 +0000

    s

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,33 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = this._validateString(code, 'Code');
+    this._name = this._validateString(name, 'Name');
+  }
+
+  _validateString(value, attribute) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${attribute} must be a string`);
+    }
+    return value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = this._validateString(value, 'Code');
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = this._validateString(value, 'Name');
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
