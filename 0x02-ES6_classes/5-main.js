d7f0da872b4360c98b6800cc3528b3b985dc45f2
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-07-26 04:11:02 +0100

    Update 5-main.js

--- 0x02-ES6_classes/5-main.js
+++ 0x02-ES6_classes/5-main.js
@@ -1,15 +1,13 @@
 import Building from './5-building.js';
 
-class TestBuilding extends Building {
-  evacuationWarningMessage() {
-    return "Evacuation warning message from TestBuilding";
-  }
-}
+const b = new Building(100);
+console.log(b);
+
+class TestBuilding extends Building {}
 
 try {
-    const testBuilding = new TestBuilding(200);
-    console.log(testBuilding);
-    console.log(testBuilding.evacuationWarningMessage());
-} catch (err) {
+    new TestBuilding(200)
+}
+catch(err) {
     console.log(err);
 }
