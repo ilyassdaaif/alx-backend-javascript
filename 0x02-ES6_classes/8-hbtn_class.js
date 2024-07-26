3855b43902037aa6c928c4f0b01f10ba2a30441e
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-07-27 00:59:02 +0100

    Update 8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,15 +1,31 @@
 export default class HolbertonClass {
   constructor(size, location) {
-    this._size = size;
-    this._location = location;
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    this._location = value;
   }
 
   [Symbol.toPrimitive](hint) {
     if (hint === 'number') {
-      return this._size;
+      return this.size;
     }
     if (hint === 'string') {
-      return this._location;
+      return this.location;
     }
     return this;
   }
