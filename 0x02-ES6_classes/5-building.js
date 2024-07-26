0f80c4eb4ce73a5ebf586ff7e0c93343c83458aa
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-07-26 17:58:17 +0100

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,27 +1,21 @@
 export default class Building {
-    constructor(sqft) {
-      if (this.constructor !== Building && 
-          !this.evacuationWarningMessage) {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-      }
-      this._sqft = sqft;
-    }
-  
-    get sqft() {
-      return this._sqft;
-    }
-  }
-
   constructor(sqft) {
+    this._sqft = sqft;
+    
+    // Check if the class extending Building has implemented evacuationWarningMessage
     if (this.constructor !== Building && 
-        !this.evacuationWarningMessage) {
+        !this.constructor.prototype.hasOwnProperty('evacuationWarningMessage')) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-    this._sqft = sqft;
   }
 
+  // Getter for sqft
   get sqft() {
     return this._sqft;
   }
-}
 
+  // Abstract method
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
