e4158e3fe9deff8c98bf92f9a51f8cb9ab93cff6
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-07-26 04:07:54 +0100

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,23 +1,13 @@
 export default class Building {
-    constructor(sqft) {
-      this._sqft = this._validateNumber(sqft, 'Sqft');
-      if (new.target === Building) {
-        throw new Error("Building is an abstract class and cannot be instantiated directly");
-      }
-    }
-  
-    _validateNumber(value, attribute) {
-      if (typeof value !== 'number') {
-        throw new TypeError(`${attribute} must be a number`);
-      }
-      return value;
-    }
-  
-    get sqft() {
-      return this._sqft;
-    }
-  
-    evacuationWarningMessage() {
+  constructor(sqft) {
+    if (this.constructor !== Building && 
+        !this.evacuationWarningMessage) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-  }  
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
