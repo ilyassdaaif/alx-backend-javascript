bf480a849e7ed37cf9d03b0c59d211d8dfac59ea
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-07-27 02:30:31 +0100

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,13 +1,9 @@
 export default function createIteratorObject(report) {
-  const departments = Object.values(report.allEmployees);
-  
-  return {
-    *[Symbol.iterator]() {
-      for (const department of departments) {
-        for (const employee of department) {
-          yield employee;
-        }
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
       }
     }
-  };
+  }());
 }
