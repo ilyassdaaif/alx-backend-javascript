a1cd4d811fbd3485854f00e0ebae81f50ca78ff3
ilyassdaaif <ilyassdf@gmail.com>
2024-07-29 20:40:25 +0100

    0x03 ES6 data manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+function getListStudents() {
+    return [
+      { id: 1, firstName: 'Guillaume', location: 'San francisco' },
+      { id: 2, firstName: 'James', location: 'Columbia' },
+      { id: 5, firstName: 'Serena', location: 'San francisco' }
+    ];
+}
+
+export default getListStudents;
