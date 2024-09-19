f410743453117595460ac3f5081c2ace6881877a
ilyassdf <ilyass@gmail.com>
2024-09-19 19:49:17 +0100

    d

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,10 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 });
 
-app.listen(1245, () => {
-  console.log('Server running on port 1245');
-});
+app.listen(1245)
 
 module.exports = app;
