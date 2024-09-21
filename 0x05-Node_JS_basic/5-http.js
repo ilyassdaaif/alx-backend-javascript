a05e3248685eaf34791a49ea1365c1a6be675fc7
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-09-21 20:12:01 +0100

    Update 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -30,7 +30,7 @@ const countStudents = async (path) => {
 
 const app = http.createServer(async (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-
+  
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
