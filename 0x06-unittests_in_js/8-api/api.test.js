e6159c0273a069c416d815b7f9fdb18b3d18c5ea
ilyassdf <ilyassdf@gmail.com>
2024-09-26 01:16:07 +0100

    p

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,35 +1,20 @@
 const request = require('request');
 const { expect } = require('chai');
+const app = require('./api'); // Import the express app
 
 describe('Index page', () => {
-  const API_URL = 'http://localhost:7865';
+  const url = 'http://localhost:7865/';
 
   it('Correct status code?', (done) => {
-    request.get(API_URL, (error, response, body) => {
+    request(url, (error, response, body) => {
       expect(response.statusCode).to.equal(200);
       done();
     });
   });
 
   it('Correct result?', (done) => {
-    request.get(API_URL, (error, response, body) => {
+    request(url, (error, response, body) => {
       expect(body).to.equal('Welcome to the payment system');
-      done();
-    });
-  });
-
-  it('Other: Content-Type is text/html?', (done) => {
-    request.get(API_URL, (error, response, body) => {
-      expect(response.headers['content-type']).to.include('text/html');
-      done();
-    });
-  });
-
-  it('Other: Response time is less than 500ms?', (done) => {
-    const startTime = Date.now();
-    request.get(API_URL, (error, response, body) => {
-      const endTime = Date.now();
-      expect(endTime - startTime).to.be.below(500);
       done();
     });
   });
