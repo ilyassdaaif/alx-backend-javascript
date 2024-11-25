605655aa10244eb658ad6ce5132a963ca52787fd
ilyassdaaif <ilyass.df.01@gmail.com>
2024-11-25 19:23:53 +0000

    0x05 Node JS basic

--- 0x05-Node_JS_basic/.eslintrc.js
+++ 0x05-Node_JS_basic/.eslintrc.js
@@ -1,16 +1,37 @@
-{
-  "env": {
-    "es2021": true,
-    "node": true
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
   },
-  "extends": "airbnb-base",
-  "parserOptions": {
-    "ecmaVersion": 12
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  "rules": {
-    "no-console": "off",
-    "no-shadow": "off",
-    "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
-    "no-param-reassign": ["error", { "props": false }]
-  }
-}
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
