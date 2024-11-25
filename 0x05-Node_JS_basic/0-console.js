622bb93a9369b8493eb732f705d59f9de6ea9d45
ilyassdaaif <ilyass.df.01@gmail.com>
2024-11-25 20:41:58 +0000

    nodejs

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,10 @@
-function displayMessage(data) {
-  console.log(data);
+/**
+ * Display a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author ilyass daaif <https://github.com/ilyassdaaif>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
 };
 
 module.exports = displayMessage;
