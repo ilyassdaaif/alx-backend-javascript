56222385974178ff39b18a7a3f281fe07bdf613d
ilyassdaaif <ilyass.df.01@gmail.com>
2024-11-26 22:34:51 +0000

    nodejs

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
 /**
  * Display a message on the STDOUT.
  * @param {String} msg The message to display.
- * @author ilyass daaif <https://github.com/ilyassdaaif>
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
  */
 const displayMessage = (msg) => {
   console.log(msg);
